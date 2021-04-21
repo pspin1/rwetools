a8943f0c8cb2c955d9afd099c41353bf8598ef83
Paul Spin <paul.spin@dal.ca>
2021-04-21 12:39:41 -0300

    Added ICD codes and GEMs to package (#1)
    Co-authored-by: Paul Spin <paul.spin@eversana.com>

---
+++ R/00_data.R
@@ -0,0 +1,66 @@
+#' ICD9CM medical codes (2014 edition)
+#'
+#' A dataset of ICD9CM codes for 2014. Courtesy of the `icd` package.
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{code}{ICD9CM code.}
+#'   \item{leaf}{Logical indicating whether code is billable}
+#'   \item{short_desc}{Short literal description of code}
+
+#'   ...
+#' }
+
+"icd9_2014"
+
+
+#' ICD10CM medical codes (2019 edition)
+#'
+#' A dataset of ICD10CM codes for 2019. Courtesy of the `icd` package.
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{code}{ICD10CM code.}
+#'   \item{leaf}{Logical indicating whether code is billable}
+#'   \item{short_desc}{Short literal description of code}
+
+#'   ...
+#' }
+
+"icd10_2019"
+
+
+#' General equivalence mapping from ICD10CM to ICD9CM (2018 edition)
+#'
+#' A dataset mapping ICD10CM codes to ICD9CM codes (2018 edition)
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{icd10}{ICD10CM code.}
+#'   \item{icd9}{ICD9CM code.}
+
+#'   ...
+#' }
+
+"gem_10_to_9"
+
+
+#' General equivalence mapping from ICD9CM to ICD10CM (2018 edition)
+#'
+#' A dataset mapping ICD9CM codes to ICD10CM codes (2018 edition)
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{icd9}{ICD9CM code.}
+#'   \item{icd10}{ICD10CM code.}
+
+#'   ...
+#' }
+
+"gem_9_to_10"
+
+
