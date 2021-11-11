fb5991c0e160d79930354c1b13355475575b7012
pspin1 <pauljspin@gmail.com>
2021-11-11 18:05:45 -0400

    slight modification to datatable call in server-side

--- inst/apps/icd_lookup/app.r
+++ inst/apps/icd_lookup/app.r
@@ -1,4 +1,4 @@
-devtools::load_all(".")
+#devtools::load_all(".")
 library(shiny)
 library(DT)
 library(shinyalert)
@@ -9,35 +9,36 @@ library(shinydashboard)
 header <- dashboardHeader(title = 'ICD Code Lookup')
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem('dashboard', tabName = 'dashboard', icon = icon('dashboard'))
+    menuItem('dashboard', tabName = 'dashboard', icon = icon('dashboard')),
+    menuItem('dashboard2', tabname = 'dashboard2')
   )
 )
 body <- dashboardBody(
-  fluidPage(fluidRow(
-    column(2,
-           textInput("fileName", "File Name",
-                       value = paste0("my file")
-           )
-    ),
-    useShinyalert(),
-    column(2,
-            selectInput("fileType", "File Type",
-                        choices = c("csv"=".csv", "xlsx"=".xlsx")
-            )
-    ),
-    useShinyalert(),
-    column(2,
-           downloadButton("downloadData", "Download Selected Rows", icon = icon("download"),
-                          style="color: #333; background-color: #FFF; border-color: #333")),
-    useShinyalert()
-    # column(2,
-    #        actionButton(inputId = "run", label = "Write Selected Rows to SQL", icon = icon("paper-plane"),
-    #                     style="color: #333; background-color: #FFF; border-color: #333")),
-    #useShinyalert()
-  )),
+  # fluidPage(fluidRow(
+  #   column(2,
+  #          textInput("fileName", "File Name",
+  #                      value = paste0("my file")
+  #          )
+  #   ),
+  #   useShinyalert(),
+  #   column(2,
+  #           selectInput("fileType", "File Type"
+  #                       choices = c("csv"=".csv", "xlsx"=".xlsx")
+  #           )
+  #   ),
+  #   useShinyalert(),
+  #   column(2,
+  #          downloadButton("downloadData", "Download Selected Rows", icon = icon("download"),
+  #                         style="color: #333; background-color: #FFF; border-color: #333")),
+  #   useShinyalert()
+  #   # column(2,
+  #   #        actionButton(inputId = "run", label = "Write Selected Rows to SQL", icon = icon("paper-plane"),
+  #   #                     style="color: #333; background-color: #FFF; border-color: #333")),
+  #   #useShinyalert()
+  # )),
   p(),
   box(
-    title = 'box', width = NULL, status = 'primary',
+    title = 'ICD Codes', width = NULL, status = 'primary',
     DT::dataTableOutput('table2')
   )
 )
@@ -45,25 +46,48 @@ body <- dashboardBody(
 ui<-dashboardPage(header, sidebar, body)
 
 server = function(input, output) {
-  output$table2 = DT::renderDataTable(
-    rwetools::icd10_2019,
-    filter = "top",
-    extensions = c("Buttons", "Select"),
-    options = list(
-      lengthChange = FALSE,
-      selection = "multiple",
-      buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-    )
-  )
+  output$table2 = DT::renderDataTable({
+    DT::datatable(
+      rwetools::icd10_2019,
+      filter = "top",
+      class = "compact hover row-border",
+      extensions = c("Scroller", "Buttons", "Select"),
+      options = list(
+        lengthChange = FALSE,
+        select = list(style = "multi", items = "row"),
+        buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+        deferRender = TRUE,
+        scrollY = 500,
+        scroller = TRUE,
+        dom = "BRlfrtip",
+        buttons = list(list(extend='selectAll',className='selectAll',
+                            text="select all rows",
+                            action=DT::JS("function () {
+                                var table = $('#DataTables_Table_0').DataTable();
+                                table.rows({ search: 'applied'}).deselect();
+                                table.rows({ search: 'applied'}).select();
+                }")
+        ), list(extend='selectNone',
+                text="DeselectAll",
+                action=DT::JS("function () {
+                                var table = $('#DataTables_Table_0').DataTable();
+                                table.rows({ search: 'applied'}).select();
+                                table.rows({ search: 'applied'}).deselect();
+                }")
+        ))
 
-  output$downloadData <- downloadHandler(
-    filename = function() {
-      paste0(input$fileName, "_", Sys.Date(), input$fileType)
-    },
-    content = function(file) {
-      write.csv(rwetools::icd10_2019[input$table2_rows_selected,], file, row.names = FALSE)
-    }
-  )
+      ), selection="none"
+    )
+  })
+
+  # output$downloadData <- downloadHandler(
+  #   filename = function() {
+  #     paste0(input$fileName, "_", Sys.Date(), input$fileType)
+  #   },
+  #   content = function(file) {
+  #     write.csv(rwetools::icd10_2019[input$table2_rows_selected,], file, row.names = FALSE)
+  #   }
+  # )
 
 }
 
